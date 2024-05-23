c046d8b783b4375d7a3fc7dcd4996af7dc26cf43
Luis-eduardo-sl <luis.lucena@aluno.ifsp.edu.br>
2024-05-23 14:29:11 -0300

    change fontweight

--- frontEnd/components/Body.js
+++ frontEnd/components/Body.js
@@ -49,7 +49,7 @@ const styles = StyleSheet.create({
     },
     logo:{
       fontSize:40,
-      fontWeight: 700,
+      fontWeight: "700",
       color: '#123DDB',
       textAlign: 'center',
     }
