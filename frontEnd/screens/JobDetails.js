53ade3b676d3b5504f1783840cfe1409e0c41e79
Luis-eduardo-sl <luis.lucena@aluno.ifsp.edu.br>
2024-05-23 17:50:36 -0300

    add inputs

--- frontEnd/screens/JobDetails.js
+++ frontEnd/screens/JobDetails.js
@@ -1,33 +1,69 @@
-// JobDetails.js
-import React from 'react';
-import { View, Text,  StyleSheet, Image, ScrollView } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text,  StyleSheet, Image, ScrollView, TextInput } from 'react-native';
 import Button from '../components/ui/Button';
 
-
 const JobDetails = ({ route }) => {
-//   const { job } = route.params;
-const { job } = route.params;
+  const { job } = route.params;
+
+  // Adicione estados para cada campo que deseja tornar editável
+  const [companyFunction, setCompanyFunction] = useState(job.companyFunction);
+  const [companyName, setCompanyName] = useState(job.companyName);
+  const [companyLocation, setCompanyLocation] = useState(job.companyLocation);
+  const [salary, setSalary] = useState(job.salary);
+  const [jobDescription, setJobDescription] = useState(job.jobDescription);
+
+  // Função para lidar com a edição do trabalho
+  const handleEditJob = () => {
+    // Aqui você pode fazer a chamada API para editar o trabalho
+  };
+
+  // Função para lidar com a exclusão do trabalho
+  const handleDeleteJob = () => {
+    // Aqui você pode fazer a chamada API para excluir o trabalho
+  };
 
   return (
     <View style={styles.container}>
-    <View style={styles.logoContainer}>
-      <Image source={{ uri: job.companyLogo }} style={styles.logo} />
-    </View>
-    <ScrollView>
-      <View style={styles.textContainer}>
-        <View style={styles.titleLocationContainer}>
-          <Text style={styles.title}>{job.companyFunction}</Text>
-          <Text style={styles.companyLocation}>{job.companyLocation}</Text>
-        </View>
-        <Text style={styles.companyName}>{job.companyName}</Text>
-        <Text style={styles.salary}>R$ {job.salary}</Text>
-        <Text style={styles.jobDescription}>{job.jobDescription}</Text>
+      <View style={styles.logoContainer}>
+        <Image source={{ uri: job.companyLogo }} style={styles.logo} />
       </View>
-    </ScrollView>
-    <View style={styles.buttonContainer}>
-      <Button title="Candidatar-se à vaga" onPress={()=>{}} />
+      <ScrollView>
+        <View style={styles.textContainer}>
+          <View style={styles.titleLocationContainer}>
+            <TextInput
+              style={styles.title}
+              value={companyFunction}
+              onChangeText={setCompanyFunction}
+            />
+            <TextInput
+              style={styles.companyLocation}
+              value={companyLocation}
+              onChangeText={setCompanyLocation}
+            />
+          </View>
+          <TextInput
+            style={styles.companyName}
+            value={companyName}
+            onChangeText={setCompanyName}
+          />
+          <TextInput
+            style={styles.salary}
+            value={salary}
+            onChangeText={setSalary}
+          />
+          <TextInput
+            style={styles.jobDescription}
+            value={jobDescription}
+            onChangeText={setJobDescription}
+            multiline
+          />
+        </View>
+      </ScrollView>
+      <View style={styles.buttonContainer}>
+        <Button title="Editar Vaga" onPress={handleEditJob} />
+        <Button title="Excluir Vaga" onPress={handleDeleteJob} />
+      </View>
     </View>
-  </View>
   );
 }
 
@@ -56,36 +92,52 @@ const styles = StyleSheet.create({
         fontSize: 24,
         fontWeight: 'bold',
         marginBottom: 10,
+        borderWidth: 1,
+        borderRadius:5,
+        borderColor: '#ddd'
         
       },
       companyName: {
         fontSize: 20,
         marginBottom: 10,
+        borderWidth: 1,
+        borderRadius:5,
+        borderColor: '#ddd'
+
       },
       companyLocation: {
         fontSize: 16,
         marginBottom: 10,
         marginLeft: 20,
-        marginTop: 9
+        marginTop: 9,
+        borderWidth: 1,
+        borderRadius:5,
+        borderColor: '#ddd'
       },
       salary: {
         fontSize: 16,
         marginBottom: 10,
-        color: "#123DDB"
+        color: "#123DDB",
+        borderWidth: 1,
+        borderRadius:5,
+        borderColor: '#ddd'
       },
       jobDescription: {
         fontSize: 14,
         marginBottom: 20,
+        borderWidth: 1,
+        borderRadius:5,
+        borderColor: '#ddd'
       },
       titleLocationContainer: {
         flexDirection: 'row',
         justifyContent: 'space-between',
       },
       buttonContainer: {
-        // flex: 1, 
         alignItems: 'center',
         justifyContent: 'flex-end', 
-        marginBottom: 20, 
+        marginBottom: -10, 
+        marginTop: 10
       },
 });
 
